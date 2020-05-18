aecd04004ba0f0d999beca42f22fef2cb0461413
kasedy <kasedy@gmail.com>
2020-05-17 18:11:17 -0700

    Moved all stuff into Device class. All members are statistically created to have less memory fragmentation.

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,19 @@
-#include <ESP8266React.h>
 #include <FS.h>
 
-#include "effects.h"
-#include "CapacitiveSensorButtonManager.h"
-#include "HomeAssistantMqttBroker.h"
-#include "HomeAssistantMqttService.h"
-#include "LightControllerService.h"
+#include "Device.h"
 
-AsyncWebServer *webServer;
-ESP8266React *esp8266React;
-LightControllerService *lightControllerService;
-HomeAssistantMqttService *homeAssistantMqttService;
-HomeAssistantMqttBroker *homeAssistantMqttBroker;
-CapacitiveSensorButtonManager *capacitiveSensorButtonManager;
+Device *device;
 
 void setup() {
 #if LOGGING
   Serial.begin(74880, SERIAL_8N1, SERIAL_TX_ONLY);
   Serial.setDebugOutput(true);
 #endif 
-  webServer = new AsyncWebServer(80);
-  esp8266React = new ESP8266React(webServer, &SPIFFS);
-  lightControllerService = new LightControllerService(LED_PINS, EFFECT_LIST, webServer);
-  homeAssistantMqttService = new HomeAssistantMqttService(lightControllerService, webServer, &SPIFFS, esp8266React->getSecurityManager());
-  homeAssistantMqttBroker = new HomeAssistantMqttBroker(lightControllerService, homeAssistantMqttService, esp8266React->getMqttClient());
-  capacitiveSensorButtonManager = new CapacitiveSensorButtonManager(CAPACITIVE_SENSOR_SEND_PIN, CAPACITIVE_SENSOR_RECEIVE_PIN, lightControllerService);
-
   SPIFFS.begin();
-  esp8266React->begin();
-  homeAssistantMqttService->begin();
-  webServer->begin();
+
+  device = new Device(&SPIFFS);
 }
 
 void loop() {
-  esp8266React->loop();
-  capacitiveSensorButtonManager->loop();
-  lightControllerService->loop();
+  device->loop();
 }
