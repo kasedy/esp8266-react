cbca7d64daa549a315c1232646c892dc5df2e1af
kasedy <kasedy@gmail.com>
2020-05-19 05:11:59 -0700

    improved debug string memory consumption

--- src/dbg.h
+++ src/dbg.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <HardwareSerial.h>
+#include <pgmspace.h>
 
 #ifndef LOGGING
   #define LOGGING false
 #endif
 
 #if LOGGING
-  #define DBG(fmt, ...) Serial.printf(fmt, ##__VA_ARGS__)
+  #define DBG(fmt, ...) Serial.printf_P(PSTR(fmt), ##__VA_ARGS__)
 #else
   #define DBG(fmt, ...)
 #endif
